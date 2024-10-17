e11a5395d944af0b7a9ba4ee10faa6af559c1ec3
shaon <shahriar.rahman@brainstation-23.com>
2024-10-17 17:35:55 +0600

    header setup

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:movie_base/app/core/values/app_values.dart';
+import 'package:movie_base/app/core/values/text_styles.dart';
 import 'package:movie_base/app/modules/home/widget/item_github_project.dart';
 import '/app/core/base/base_view.dart';
 import '/app/modules/home/controllers/home_controller.dart';
@@ -12,10 +14,17 @@ class HomeView extends BaseView<HomeController> {
 
   @override
   Widget body(BuildContext context) {
-    return Column(
-      children: [
-        MovieHeader()
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(AppValues.padding),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text('Welcome Back', style: primaryColorSubtitleStyle),
+          const Text('Shahriar Rahman', style: centerTextStyle,),
+          const SizedBox(height: 20,),
+          MovieHeader(),
+        ],
+      ),
     );
   }
 }
--- lib/app/modules/home/widget/item_github_project.dart
+++ lib/app/modules/home/widget/item_github_project.dart
@@ -1,33 +1,96 @@
 import 'package:flutter/material.dart';
-import 'package:movie_base/app/core/widget/ripple.dart';
-import 'package:get/get.dart';
+import 'package:movie_base/app/core/values/app_values.dart';
 import '/app/core/base/base_widget_mixin.dart';
-import '/app/core/values/app_colors.dart';
-import '/app/core/values/app_values.dart';
-import '/app/core/values/text_styles.dart';
-import '/app/core/widget/elevated_container.dart';
-import '/app/core/widget/icon_text_widgets.dart';
-import '/app/modules/home/model/github_project_ui_data.dart';
-import '/app/routes/app_pages.dart';
 
 class MovieHeader extends StatelessWidget with BaseWidgetMixin {
-  //final GithubProjectUiData dataModel;
-
   MovieHeader({
     Key? key,
-    //required this.dataModel,
   }) : super(key: key);
 
   @override
   Widget body(BuildContext context) {
-    return ElevatedContainer(
-        child: Expanded(
-            child: Text('ts')
-        ),
+    final double height = MediaQuery.of(context).size.height;
+    final double width = MediaQuery.of(context).size.width;
+
+    return Stack(
+      children: [
+        _buildMovieImage(width, height),
+        _buildRatingBox(),
+        _buildMovieDetailsOverlay(width, height),
+      ],
     );
   }
 
+  Widget _buildMovieImage(double width, double height) {
+    return Container(
+      width: width,
+      height: height / 4,
+      child: Image.network(
+        'https://yts.mx/assets/images/movies/fast_forward_1985/large-cover.jpg',
+        fit: BoxFit.fill,
+      ),
+    );
+  }
 
-    //Get.toNamed(Routes.PROJECT_DETAILS, arguments: dataModel);
+  Widget _buildRatingBox() {
+    return Positioned(
+      top: AppValues.padding,
+      left: AppValues.padding,
+      child: Container(
+        padding: EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: Colors.black.withOpacity(0.7),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: const Row(
+          children: [
+            Icon(
+              Icons.star,
+              color: Colors.yellow,
+              size: AppValues.padding,
+            ),
+            SizedBox(width: 4),
+            Text(
+              '8.5',
+              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
+  Widget _buildMovieDetailsOverlay(double width, double height) {
+    return Positioned(
+      bottom: 0,
+      child: Container(
+        width: width,
+        height: height / 10,
+        color: Colors.black.withOpacity(0.3),
+        padding: const EdgeInsets.all(AppValues.padding),
+        child: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Fast Forward (1985)',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: AppValues.padding,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 4),
+            Text(
+              'Action, Drama | 1h 34m',
+              style: TextStyle(
+                color: Colors.white70,
+                fontSize: AppValues.padding,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
